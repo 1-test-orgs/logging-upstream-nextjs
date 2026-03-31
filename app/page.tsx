62e83ad7ba3f4488164b2b6cd62ff2c7ae3ea44b
Aniket Biprojit Chowdhury <8.aniket.chowdhury@gmail.com>
2026-03-31 19:21:28 +0530

    fix: updated package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import packageJson from "../package.json";
+
 export default function Home() {
-  return <div>Welcome to Pantheon Platform</div>;
+  return <div>Welcome to Pantheon Platform {packageJson.version}</div>;
 }
